d7a774c7a6c747a50aaac171210d89fed774f229
wunzt <wunzt@oregonstate.edu>
2022-05-03 22:09:49 -0700

    Assignment 6.b

--- add_surname.py
+++ add_surname.py
@@ -4,8 +4,12 @@
 # Description: Returns a list of names, from a larger list, that start with K and adds the surname Kardashian.
 from typing import List
 
-names = []
+def add_surname(names):
 
-for string in names:
-    if string[0] == "K":
-        print(string + " Kardashian")
+    Kardashians = []
+
+    for string in names:
+        if string[0] == "K":
+            Kardashians += [string + " Kardashian"]
+
+    return Kardashians
