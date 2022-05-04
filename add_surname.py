c1a6aa573abb2aef1a863db8b4c0d2a43260fe4d
wunzt <wunzt@oregonstate.edu>
2022-05-03 21:52:59 -0700

    Assignment 6.b

---
+++ add_surname.py
@@ -0,0 +1,9 @@
+# Author: Thomas Wunz
+# GitHub username: wunzt
+# Date: 5/2/2022
+# Description: Returns a list of names, from a larger list, that start with K and adds the surname Kardashian.
+from typing import List
+
+for string in names:
+    if string[0] == "K":
+        print(string + " Kardashian")
